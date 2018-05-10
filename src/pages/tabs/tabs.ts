040a8e4a29f6a3e683c38db7158260e002250e0c
Danyllo <danyllo_harper@hotmail.com>
2018-05-10 08:05:00 -0300

    Pizzaria

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,15 +3,18 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { IntroPage } from '../intro/intro';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = IntroPage;
+  tab2Root = HomePage;
+  tab3Root = AboutPage;
+  tab4Root = ContactPage;
+  
 
   constructor() {
 
